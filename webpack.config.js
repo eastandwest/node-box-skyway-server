9ce8562fd20537f83300ea4951c6676394a0e472
Toshiyuki Hayashi <thayashing@gmail.com>
2016-09-06 12:28:30 -0700

    UI Update

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
-var webpack = require('webpack')
+var webpack = require('webpack');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 var path = require('path')
   , _entry;
@@ -33,7 +34,23 @@ module.exports = {
         }
       },
       { test: /\.html$/, loader: 'raw-loader' },
-      { test: /\.json$/, loader: 'json-loader' }
+      { test: /\.json$/, loader: 'json-loader' },
+      {
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap')
+      },
+      {
+        test: /\.scss$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!sass?sourceMap')
+      },
+      {
+        test: /\.less$/,
+        loader: ExtractTextPlugin.extract('style-loader', 'css-loader?sourceMap!less?sourceMap')
+      },
+      {
+        test: /\.(woff|svg|ttf|eot)([\?]?.*)$/,
+        loader: 'file-loader?name=[name].[ext]'
+      }
     ]
   },
   devServer:{
